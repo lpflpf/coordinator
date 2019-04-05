659a98651695c3dbb1c9f186465099a64114f123
lipengfei5 <lipengfei5@360.cn>
2019-04-05 17:05:00 +0800

    add Simple Sharding/Strategy.

--- sharding.go
+++ sharding.go
@@ -1,6 +1,19 @@
 package coordinator
 
+import "encoding/json"
+
 type Sharding interface {
 	Encode() []byte
 	Decode([]byte)
+}
+
+type SimpleSharding map[string][]int64
+
+func (sharding SimpleSharding) Encode() []byte {
+	data, _ := json.Marshal(sharding)
+	return data
+}
+
+func (sharding SimpleSharding) Decode(data []byte) {
+	_ = json.Unmarshal(data, &sharding)
 }
--- strategy.go
+++ strategy.go
@@ -3,3 +3,32 @@ package coordinator
 type Strategy interface {
 	ReBalance(sharding Sharding, currentLiveNodes []string) Sharding
 }
+
+type SimpleStrategy struct{}
+
+func (strategy SimpleStrategy) ReBalance(sharding Sharding, currentLiveNodes []string) Sharding {
+	simpleSharding := sharding.(SimpleSharding)
+
+	if len(currentLiveNodes) == len(simpleSharding) {
+		return sharding
+	}
+
+	newSimpleSharding := SimpleSharding{}
+	for _, node := range currentLiveNodes {
+		newSimpleSharding[node] = []int64{}
+	}
+
+	var allJobs []int64
+	for _, jobs := range simpleSharding {
+		allJobs = append(allJobs, jobs...)
+	}
+
+	iter := 0
+	for _, job := range allJobs {
+		iter = (iter) % len(currentLiveNodes)
+		node := currentLiveNodes[iter]
+		newSimpleSharding[node] = append(newSimpleSharding[node], job)
+	}
+
+	return newSimpleSharding
+}
